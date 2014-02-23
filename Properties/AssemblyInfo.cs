c5c943094ad79ac99499ea624f4c8a13181d0230
Karl Waclawek <karl@waclawek.net>
2014-02-23 13:03:02 -0500

    Added External libraries.

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("KdSoft.StorageBase")]
+[assembly: AssemblyDescription("Base for Key/Value based storage services.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("KD Soft")]
+[assembly: AssemblyProduct("KD Soft Storage Service")]
+[assembly: AssemblyCopyright("Copyright © Karl Waclawek 2007")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("e23b0937-081a-4e87-a627-5bfd014d4a04")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
