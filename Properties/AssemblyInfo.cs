1654390c036ca87fce5ec8d34ea2b92b72876fec
Karl Waclawek <karl@waclawek.net>
2014-02-23 13:03:02 -0500

    Added External libraries.

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,40 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("KdSoft.TransientStorage")]
+[assembly: AssemblyDescription("Provides Key/Value based in-memory storage services.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("KD Soft")]
+[assembly: AssemblyProduct("KD Soft Storage Service")]
+[assembly: AssemblyCopyright("Copyright © Karl Waclawek 2007")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("61e15f4b-aaae-4526-8d18-d94ab97ca1c8")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+
+// http://www.sturmnet.org/blog/2005/05/10/internalsvisibleto-sn
+[assembly: InternalsVisibleTo("Test, PublicKeyToken=<4327aaa486c60d1c>")]
+
---
+++ TransientStorageManager.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Runtime;
+
+namespace KdSoft.Services.StorageServices.Transient
+{
+  /// <summary>Transient storage specific store manager.</summary>
+  /// <remarks>Exposes transient storage specific methods.</remarks>
+  public class TransientStorageManager: StorageManager<TransientStore>, IDisposable
+  {
+    Timer checkTimer;
+    int lastMemoryCheck;
+    bool memoryLow = false;
+    object checkTimerObj = new object();
+
+    public TransientStorageManager() : base() {
+      // default values
+      LockCheckWait = TimeSpan.FromMilliseconds(500);
+      MemoryCheckPeriod = TimeSpan.FromSeconds(10);
+      lastMemoryCheck = Environment.TickCount;
+      checkTimer = new Timer(CheckTimerHandler);
+      checkTimer.Change(LockCheckWait, LockCheckWait);
+    }
+
+    void CheckTimerHandler(object state) {
+      if (!Monitor.TryEnter(checkTimerObj))
+        return;
+      try {
+        memoryLow = !ProcessCheckTimer();
+        if (memoryLow) {
+          GC.Collect();
+          memoryLow = !CheckMemory();
+        }
+      }
+      finally {
+        Monitor.Exit(checkTimerObj);
+      }
+    }
+
+    bool CheckMemory() {
+      bool result = true;
+      try {
+        // Let's try to determine if we have enough memory available. Here we assume
+        // each entry in a property storage dictionary uses 16 bytes, so if we have
+        // 2^20 (1M) entries, we use 16 MBytes and if the dictionary re-hashes it will
+        // concurrently allocate at least twice its size (for the next size increase), so
+        // for 2^20 (1M = 1048576) entries we need to be able to allocate at least 32 MBytes.
+        int requiredMemory = EntryCount * 32 / 1048576;
+        if (requiredMemory < 2)
+          requiredMemory = 2;
+        using (new MemoryFailPoint(requiredMemory)) { }  // very slow
+      }
+      catch (InsufficientMemoryException) {
+        result = false;
+      }
+      lastMemoryCheck = Environment.TickCount;
+      return result;
+    }
+
+    bool ProcessCheckTimer() {
+      ProcessCheckEvent();
+      // check memory (once in a while) and trim the wait queue
+      TimeSpan elapsed = new TimeSpan(0, 0, 0, 0, (int)(Environment.TickCount - lastMemoryCheck));
+      if (elapsed > MemoryCheckPeriod) {  // consider possible overflow condition
+        // TrimMemory();
+        return CheckMemory();
+      }
+      else
+        return true;
+    }
+
+    #region Public API
+
+    public TimeSpan LockCheckWait { get; set; }
+
+    public TimeSpan MemoryCheckPeriod { get; set; }
+
+    public void ProcessCheckEvent() {
+      TransientStore[] storeList;
+      lock (storeLock) {
+        storeList = new TransientStore[stores.Count];
+        stores.Values.CopyTo(storeList, 0);
+      }
+
+      for (int indx = 0; indx < storeList.Length; indx++) {
+        var store = storeList[indx];
+        store.ProcessLockWaitQueue();
+        store.ProcessTimeOuts();
+      }
+    }
+
+    public int EntryCount {
+      get {
+        int result = 0;
+        lock (storeLock) {
+          foreach (KeyValuePair<string, TransientStore> entry in stores) {
+            result += entry.Value.EntryCount;
+          }
+        }
+        return result;
+      }
+    }
+
+    #endregion
+
+    #region IDisposable Members
+
+    public void Dispose() {
+      try {
+        CloseStores();
+      }
+      finally {
+        checkTimer.Dispose();
+      }
+    }
+
+    #endregion
+  }
+}
