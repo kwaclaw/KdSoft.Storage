72d5498886d8cc3d8c6b6331856abe245911f811
Karl Waclawek <karl@waclawek.net>
2014-12-01 16:21:41 -0500

    [External/KdSoft.*] Added RemoveAll extenstion to IList<T>. Made build and revision numbers in version auto-increment.

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -32,8 +32,8 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyFileVersion("1.0.*")]
 
 // http://www.sturmnet.org/blog/2005/05/10/internalsvisibleto-sn
 [assembly: InternalsVisibleTo("Test, PublicKeyToken=<4327aaa486c60d1c>")]
