1b5fec6f2b3fbfc601e42740e66f0448cb5eb12d
Karl Waclawek <karl.waclawek@qlinesolutions.com>
2016-03-01 20:28:15 -0500

    New DNX SDK version update.

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -14,6 +14,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
+#if !COREFX
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
@@ -21,3 +22,4 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("7eabedab-c8ae-43e2-90c1-fcc880d160fc")]
+#endif
